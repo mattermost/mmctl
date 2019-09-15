2874a16c5ecb08c282e273dfa39a5211e2fcd88f
Miguel de la Cruz <miguel@mcrx.me>
2019-09-15 19:24:31 +0200

    Add new logger and migrate team list command

--- commands/root.go
+++ commands/root.go
@@ -7,6 +7,7 @@ import (
 type Command = cobra.Command
 
 func Run(args []string) error {
+	RootCmd.PersistentFlags().StringP("format", "f", "plain", "the format of the command output")
 	RootCmd.SetArgs(args)
 	return RootCmd.Execute()
 }
@@ -15,4 +16,11 @@ var RootCmd = &cobra.Command{
 	Use:   "mmctl",
 	Short: "Remote client for the Open Source, self-hosted Slack-alternative",
 	Long:  `Mattermost offers workplace messaging across web, PC and phones with archiving, search and integration with your existing systems. Documentation available at https://docs.mattermost.com`,
+	PersistentPreRun: func(command *cobra.Command, args []string) {
+		format, _ := command.Flags().GetString("format")
+		Log.SetType(format)
+	},
+	PersistentPostRun: func(command *cobra.Command, args []string) {
+		Log.Flush()
+	},
 }
