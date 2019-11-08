5d7f928b22a81a42427361a5e03f5973d1092adc
Miguel de la Cruz <miguel@mcrx.me>
2019-11-09 00:26:03 +0100

    Add tests for the search channel command

---
+++ commands/channel_test.go
@@ -0,0 +1,131 @@
+package commands
+
+import (
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mmctl/printer"
+
+	"github.com/spf13/cobra"
+)
+
+func (s *MmctlUnitTestSuite) TestSearchChannelCmdF() {
+	s.Run("Search for an existing channel on an existing team", func() {
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelArg := "example-channel"
+		mockChannel := model.Channel{Name: channelArg}
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("team", teamArg, "")
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByName(channelArg, teamArg, "").
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := searchChannelCmdF(s.client, cmd, []string{channelArg})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 1)
+		s.Equal(&mockChannel, printer.GetLines()[0])
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Search for an existing channel without specifying team", func() {
+		printer.Clean()
+		teamId := "example-team-id"
+		otherTeamId := "example-team-id-2"
+		mockTeams := []*model.Team{
+			&model.Team{Id: otherTeamId},
+			&model.Team{Id: teamId},
+		}
+		channelArg := "example-channel"
+		mockChannel := model.Channel{Name: channelArg}
+
+		s.client.
+			EXPECT().
+			GetAllTeams("", 0, 9999).
+			Return(mockTeams, &model.Response{Error: nil}).
+			Times(1)
+
+		// first call is for the other team, that doesn't have the channel
+		s.client.
+			EXPECT().
+			GetChannelByName(channelArg, otherTeamId, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		// second call is for the team that contains the channel
+		s.client.
+			EXPECT().
+			GetChannelByName(channelArg, teamId, "").
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := searchChannelCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 1)
+		s.Equal(&mockChannel, printer.GetLines()[0])
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Search for a nonexistent channel", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelArg := "example-channel"
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("team", teamArg, "")
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByName(channelArg, teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := searchChannelCmdF(s.client, cmd, []string{channelArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Channel "+channelArg+" was not found in team "+teamArg)
+	})
+
+	s.Run("Search for a channel in a nonexistent team", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		channelArg := "example-channel"
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("team", teamArg, "")
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := searchChannelCmdF(s.client, cmd, []string{channelArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Team "+teamArg+" was not found")
+	})
+}
