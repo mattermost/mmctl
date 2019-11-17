0912710699ad163dc74c15ae38ba72a4866b9e6c
Allen Lai <allenplai@gmail.com>
2019-11-16 16:41:47 -0800

    test for command

--- commands/command_test.go
+++ commands/command_test.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mmctl/printer"
-
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +10,6 @@ func (s *MmctlUnitTestSuite) TestCreateCommandCmdF() {
 	s.Run("Create a new custom slash command for a specified team", func() {
 		printer.Clean()
 		teamArg := "example-team-id"
-		mockTeam := model.Team{Id: teamArg}
 		titleArg := "example-command-name"
 		descriptionArg := "example-description-text"
 		triggerWordArg := "example-trigger-word"
@@ -19,7 +17,13 @@ func (s *MmctlUnitTestSuite) TestCreateCommandCmdF() {
 		creatorUsernameArg := "example-username"
 		responseUsernameArg := "example-username2"
 		iconArg := "icon-url"
+		method := "G"
+		autocomplete := false
+		autocompleteDesc := "autocompleteDesc"
+		autocompleteHint := "autocompleteHint"
 
+		mockTeam := model.Team{Id: teamArg}
+		mockCreator := model.User{Username: creatorUsernameArg}
 		mockCommand := model.Command{
 			CreatorId:        creatorUsernameArg,
 			TeamId:           teamArg,
@@ -29,7 +33,7 @@ func (s *MmctlUnitTestSuite) TestCreateCommandCmdF() {
 			DisplayName:      titleArg,
 			Description:      descriptionArg,
 			URL:              urlArg,
-			Method:           method,		// it's 'P' or 'G'
+			Method:           method,
 			AutoComplete:     autocomplete,
 			AutoCompleteDesc: autocompleteDesc,
 			AutoCompleteHint: autocompleteHint,
@@ -45,35 +49,77 @@ func (s *MmctlUnitTestSuite) TestCreateCommandCmdF() {
 		cmd.Flags().String("response-username", responseUsernameArg, "")
 		cmd.Flags().String("icon", iconArg, "")
 
-		// get the mock team
+		// createCommandCmdF will call getTeamFromTeamArg,  getUserFromUserArg,
 		s.client.
 			EXPECT().
 			GetTeam(teamArg, "").
 			Return(&mockTeam, &model.Response{Error: nil}).
 			Times(1)
-		
-		err := createCommandCmdF(s.client, cmd, []string{})
+		s.client.
+			EXPECT().
+			GetUserByEmail(creatorUsernameArg, "").
+			Return(&mockCreator, &model.Response{Error: nil}).
+			Times(1)
+		s.client.
+			EXPECT().
+			CreateCommand(cmd).		// ERROR ; passing in the wrong argument???
+			Return(&mockCommand, &model.Response{Error: nil}).
+			Times(1)
+
+		err := createCommandCmdF(s.client, cmd, []string{teamArg})
 		s.Require().Nil(err)
 		s.Len(printer.GetLines(), 1)
-		s.Equal(&mockChannel, printer.GetLines()[0])
+		s.Equal(&mockCommand, printer.GetLines()[0])
 		s.Len(printer.GetErrorLines(), 0)
 	})
 
-
+	/*
 	s.Run("List commands for a existing team", func() {
 		printer.Clean()
 		teamArg := "example-team-id"
 		mockTeam := model.Team{Id: teamArg}
 
 		cmd := &cobra.Command{}
-		// cmd.Flags().String("team", teamArg, "")
+		cmd.Flags().String("team", teamArg, "")
+
+
+		titleArg := "example-command-name"
+		descriptionArg := "example-description-text"
+		triggerWordArg := "example-trigger-word"
+		urlArg := "http://localhost:8000/example"
+		creatorUsernameArg := "example-username"
+		responseUsernameArg := "example-username2"
+		iconArg := "icon-url"
+		method := "G"
+		autocomplete := false
+		autocompleteDesc := "autocompleteDesc"
+		autocompleteHint := "autocompleteHint"
+		mockCommand := model.Command{
+			CreatorId:        creatorUsernameArg,
+			TeamId:           teamArg,
+			Trigger:          triggerWordArg,
+			Username:         responseUsernameArg,
+			IconURL:          iconArg,
+			DisplayName:      titleArg,
+			Description:      descriptionArg,
+			URL:              urlArg,
+			Method:           method,
+			AutoComplete:     autocomplete,
+			AutoCompleteDesc: autocompleteDesc,
+			AutoCompleteHint: autocompleteHint,
+		}
 
 		s.client.
 			EXPECT().
 			GetTeam(teamArg, "").
 			Return(&mockTeam, &model.Response{Error: nil}).
 			Times(1)
-		
+		s.client.
+			EXPECT().
+			ListCommands(teamArg, "").
+			Return(&mockCommand, &model.Response{Error: nil}).
+			Times(1)
+
 		err := listCommandCmdF(s.client, cmd, []string{teamArg})
 		s.Require().Nil(err)
 		s.Len(printer.GetLines(), 1)
@@ -86,12 +132,17 @@ func (s *MmctlUnitTestSuite) TestCreateCommandCmdF() {
 		mockTeam := model.Team{Id: teamArg}
 
 		cmd := &cobra.Command{}
-		// cmd.Flags().String("team", teamArg, "")
+		cmd.Flags().String("team", teamArg, "")
 
-		
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
 		err := deleteCommandCmdF(s.client, cmd, []string{teamArg})
 		s.Require().Nil(err)
 		s.Len(printer.GetLines(), 1)
 		s.Len(printer.GetErrorLines(), 0)
-	})
+	}) */
 }
