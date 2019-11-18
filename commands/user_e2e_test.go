1230a4f052f0068ab5ee1206fb6fead9b5b61722
Miguel de la Cruz <miguel@mcrx.me>
2019-11-18 15:55:05 +0100

    [MM-19912] Add integration tests to mmctl

---
+++ commands/user_e2e_test.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mmctl/printer"
+
+	"github.com/spf13/cobra"
+)
+
+func (s *MmctlE2ETestSuite) TestSearchUserCmd() {
+	s.Run("Search for an existing user", func() {
+		printer.Clean()
+
+		err := searchUserCmdF(s.th.Client, &cobra.Command{}, []string{s.th.BasicUser.Email})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 1)
+		user := printer.GetLines()[0].(*model.User)
+		s.Equal(s.th.BasicUser.Username, user.Username)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Search for a nonexistent user", func() {
+		printer.Clean()
+		emailArg := "nonexistentUser@example.com"
+
+		err := searchUserCmdF(s.th.Client, &cobra.Command{}, []string{emailArg})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Equal("Unable to find user '"+emailArg+"'", printer.GetErrorLines()[0])
+	})
+}
