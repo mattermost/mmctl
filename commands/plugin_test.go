03a079d744c5739225313460ed6c9a406dddb243
Jimi Olaniyan <jimiolaniyan@gmail.com>
2019-11-19 14:25:04 +0100

    Add tests for enable plugin command

--- commands/plugin_test.go
+++ commands/plugin_test.go
@@ -76,3 +76,95 @@ func (s *MmctlUnitTestSuite) TestPluginDisableCmd() {
 		s.Require().Equal(printer.GetErrorLines()[1], "Unable to disable plugin: "+args[3]+". Error: "+mockError.Error())
 	})
 }
+
+func (s *MmctlUnitTestSuite) TestPluginEnableCmd() {
+	s.Run("Enable 1 plugin", func() {
+		printer.Clean()
+		pluginArg := "test-plugin"
+
+		s.client.
+			EXPECT().
+			EnablePlugin(pluginArg).
+			Return(false, &model.Response{Error: nil}).
+			Times(1)
+
+		err := pluginEnableCmdF(s.client, &cobra.Command{}, []string{pluginArg})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetErrorLines(), 0)
+		s.Require().Len(printer.GetLines(), 1)
+		s.Require().Equal(printer.GetLines()[0], "Enabled plugin: "+pluginArg)
+	})
+
+	s.Run("Enable multiple plugins", func() {
+		printer.Clean()
+		plugins := []string{"plugin1", "plugin2", "plugin3"}
+
+		for _, plugin := range plugins {
+			s.client.
+				EXPECT().
+				EnablePlugin(plugin).
+				Return(false, &model.Response{Error: nil}).
+				Times(1)
+		}
+
+		err := pluginEnableCmdF(s.client, &cobra.Command{}, plugins)
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 3)
+		s.Require().Len(printer.GetErrorLines(), 0)
+		s.Require().Equal(printer.GetLines()[0], "Enabled plugin: "+plugins[0])
+		s.Require().Equal(printer.GetLines()[1], "Enabled plugin: "+plugins[1])
+		s.Require().Equal(printer.GetLines()[2], "Enabled plugin: "+plugins[2])
+	})
+
+	s.Run("Fail to enable plugin", func() {
+		printer.Clean()
+		pluginArg := "fail-plugin"
+		mockErr := &model.AppError{Message: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			EnablePlugin(pluginArg).
+			Return(false, &model.Response{Error: mockErr}).
+			Times(1)
+
+		err := pluginEnableCmdF(s.client, &cobra.Command{}, []string{pluginArg})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 1)
+		s.Require().Equal(printer.GetErrorLines()[0], "Unable to enable plugin: "+pluginArg+". Error: "+mockErr.Error())
+	})
+
+	s.Run("Enable multiple plugins with some having errors", func() {
+		printer.Clean()
+		okPlugins := []string{"ok-plugin-1", "ok-plugin-2"}
+		failPlugins := []string{"fail-plugin-1", "fail-plugin-2"}
+		allPlugins := append(okPlugins, failPlugins...)
+
+		mockErr := &model.AppError{Message: "Mock Error"}
+
+		for _, plugin := range okPlugins {
+			s.client.
+				EXPECT().
+				EnablePlugin(plugin).
+				Return(false, &model.Response{Error: nil}).
+				Times(1)
+		}
+
+		for _, plugin := range failPlugins {
+			s.client.
+				EXPECT().
+				EnablePlugin(plugin).
+				Return(false, &model.Response{Error: mockErr}).
+				Times(1)
+		}
+
+		err := pluginEnableCmdF(s.client, &cobra.Command{}, allPlugins)
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 2)
+		s.Require().Equal(printer.GetLines()[0], "Enabled plugin: "+okPlugins[0])
+		s.Require().Equal(printer.GetLines()[1], "Enabled plugin: "+okPlugins[1])
+		s.Require().Len(printer.GetErrorLines(), 2)
+		s.Require().Equal(printer.GetErrorLines()[0], "Unable to enable plugin: "+failPlugins[0]+". Error: "+mockErr.Error())
+		s.Require().Equal(printer.GetErrorLines()[1], "Unable to enable plugin: "+failPlugins[1]+". Error: "+mockErr.Error())
+	})
+}
