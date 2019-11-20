3cd11164effe15073f586ed3a5c54e6d777ea809
larkox <larkox@gmail.com>
2019-11-20 15:39:15 +0100

    [GH-13050] Add unit tests to "SendPasswordResetEmailCmd" to use testify (#41)
    * Add unit tests to "SendPasswordResetEmailCmd" to use testify
    
    * Use NoError instead of error = nil assertions.

--- commands/user_test.go
+++ commands/user_test.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"strings"
+
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mmctl/printer"
 
@@ -50,6 +52,76 @@ func (s *MmctlUnitTestSuite) TestSearchUserCmd() {
 		s.Require().Nil(err)
 		s.Require().Len(printer.GetLines(), 0)
 		s.Require().Equal("Unable to find user 'example@example.com'", printer.GetErrorLines()[0])
+	})
+}
+
+func (s *MmctlUnitTestSuite) TestSendPasswordResetEmailCmd() {
+	s.Run("Send one reset email", func() {
+		printer.Clean()
+		emailArg := "example@example.com"
+
+		s.client.
+			EXPECT().
+			SendPasswordResetEmail(emailArg).
+			Return(false, &model.Response{Error: nil}).
+			Times(1)
+
+		err := sendPasswordResetEmailCmdF(s.client, &cobra.Command{}, []string{emailArg})
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Send one reset email and receive error", func() {
+		printer.Clean()
+		emailArg := "example@example.com"
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			SendPasswordResetEmail(emailArg).
+			Return(false, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := sendPasswordResetEmailCmdF(s.client, &cobra.Command{}, []string{emailArg})
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 1)
+		s.Require().Equal("Unable send reset password email to email "+emailArg+". Error: "+mockError.Error(), printer.GetErrorLines()[0])
+	})
+
+	s.Run("Send several reset emails and receive some errors", func() {
+		printer.Clean()
+		emailArg := []string{
+			"example1@example.com",
+			"error1@example.com",
+			"error2@example.com",
+			"example2@example.com",
+			"example3@example.com"}
+		mockError := model.AppError{Id: "Mock Error"}
+
+		for _, email := range emailArg {
+			if strings.HasPrefix(email, "error") {
+				s.client.
+					EXPECT().
+					SendPasswordResetEmail(email).
+					Return(false, &model.Response{Error: &mockError}).
+					Times(1)
+			} else {
+				s.client.
+					EXPECT().
+					SendPasswordResetEmail(email).
+					Return(false, &model.Response{Error: nil}).
+					Times(1)
+			}
+		}
+
+		err := sendPasswordResetEmailCmdF(s.client, &cobra.Command{}, emailArg)
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 2)
+		s.Require().Equal("Unable send reset password email to email "+emailArg[1]+". Error: "+mockError.Error(), printer.GetErrorLines()[0])
+		s.Require().Equal("Unable send reset password email to email "+emailArg[2]+". Error: "+mockError.Error(), printer.GetErrorLines()[1])
 	})
 }
 
