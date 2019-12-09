ab9d5c20dead84183e5192cd3185b341adde5173
Micah Thompson <mjthomp95@gmail.com>
2019-12-09 17:18:10 -0500

    [MM-20836] Add unit tests to the "TeamGroupDisableCmd" mmctl command

---
+++ commands/group_test.go
@@ -0,0 +1,82 @@
+package commands
+
+import (
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mmctl/printer"
+
+	"github.com/spf13/cobra"
+)
+
+func (s *MmctlUnitTestSuite) TestTeamGroupDisableCmd() {
+	s.Run("Disable existing team", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(false)}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(teamArg, &teamPatch).
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Disable nonexisting team", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Unable to find team '"+teamArg+"'")
+	})
+
+	s.Run("Error response from PatchTeam", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(false)}
+		errResponse := "PatchTeam Error"
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(teamArg, &teamPatch).
+			Return(nil, &model.Response{Error: &model.AppError{Message: errResponse}}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.Contains(err.Error(), errResponse)
+	})
+}
