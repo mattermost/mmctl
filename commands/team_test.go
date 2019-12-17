38bf7167bd3db6c89017c3f4d871edc048d012d8
lebedev.vlaimir <lebedev.vladimir3@gmail.com>
2019-12-17 03:05:26 +0300

    Add unit tests to the "AddUsersCmd" mmctl command

--- commands/team_test.go
+++ commands/team_test.go
@@ -108,3 +108,153 @@ func (s *MmctlUnitTestSuite) TestCreateTeamCmd() {
 		s.Require().Len(printer.GetLines(), 0)
 	})
 }
+
+func (s *MmctlUnitTestSuite) TestAddUsersCmd() {
+	s.Run("Add users with not enough arguments returns error", func() {
+
+		cmd := &cobra.Command{}
+		err := addUsersCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err, errors.New("Not enough arguments."))
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Add users with no team in arguments returns error", func() {
+
+		cmd := &cobra.Command{}
+
+		s.client.
+			EXPECT().
+			GetTeam("team1", "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName("team1", "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := addUsersCmdF(s.client, cmd, []string{"team1", "user1"})
+		s.Require().Equal(err, errors.New("Unable to find team 'team1'"))
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Add users with no existed user in arguments prints error", func() {
+		printer.Clean()
+		cmd := &cobra.Command{}
+		mockTeam := model.Team{
+			Id:          "TeamId",
+			Name:        "team1",
+			DisplayName: "DisplayName",
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam("team1", "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUserByEmail("user1", "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUserByUsername("user1", "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUser("user1", "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := addUsersCmdF(s.client, cmd, []string{"team1", "user1"})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetErrorLines(), 1)
+		s.Require().Equal(printer.GetErrorLines()[0], "Can't find user 'user1'")
+	})
+
+	s.Run("Add users should print error when cannot add team member", func() {
+		printer.Clean()
+		cmd := &cobra.Command{}
+		mockTeam := model.Team{
+			Id:          "TeamId",
+			Name:        "team1",
+			DisplayName: "DisplayName",
+		}
+
+		mockUser := model.User{
+			Id:       "UserID",
+			Username: "ExampleUser",
+			Email:    "example@example.com",
+		}
+		s.client.
+			EXPECT().
+			GetTeam("team1", "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUserByEmail("user1", "").
+			Return(&mockUser, &model.Response{Error: nil}).
+			Times(1)
+
+		mockError := &model.AppError{Message: "Cannot add team member"}
+
+		s.client.
+			EXPECT().
+			AddTeamMember("TeamId", "UserID").
+			Return(nil, &model.Response{Error: mockError}).
+			Times(1)
+
+		err := addUsersCmdF(s.client, cmd, []string{"team1", "user1"})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetErrorLines(), 1)
+		s.Require().Equal(printer.GetErrorLines()[0], "Unable to add 'user1' to team1. Error: : Cannot add team member, ")
+	})
+
+	s.Run("Add users should not print in console anything on success", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		mockTeam := model.Team{
+			Id:          "TeamId",
+			Name:        "team1",
+			DisplayName: "DisplayName",
+		}
+
+		mockUser := model.User{
+			Id:       "UserID",
+			Username: "ExampleUser",
+			Email:    "example@example.com",
+		}
+		s.client.
+			EXPECT().
+			GetTeam("team1", "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUserByEmail("user1", "").
+			Return(&mockUser, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			AddTeamMember("TeamId", "UserID").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := addUsersCmdF(s.client, cmd, []string{"team1", "user1"})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+}
