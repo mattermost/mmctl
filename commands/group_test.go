f824bff11278f4095a2e34663be5cc623a056f8d
larkox <larkox@gmail.com>
2019-12-19 18:53:31 +0100

    [GH-13289] Add unit tests to the "TeamGroupEnableCmd" mmctl command (#83)
    * Add unit tests to the "TeamGroupEnableCmd" mmctl command
    
    * Remove comments and use less variables

---
+++ commands/group_test.go
@@ -0,0 +1,168 @@
+package commands
+
+import (
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mmctl/printer"
+	"github.com/spf13/cobra"
+)
+
+func (s *MmctlUnitTestSuite) TestTeamGroupEnableCmd() {
+	s.Run("Enable unexisting team", func() {
+		printer.Clean()
+
+		arg := "teamId"
+
+		s.client.
+			EXPECT().
+			GetTeam(arg, "").
+			Return(nil, &model.Response{Error: &model.AppError{}}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(arg, "").
+			Return(nil, &model.Response{Error: &model.AppError{}}).
+			Times(1)
+
+		err := teamGroupEnableCmdF(s.client, &cobra.Command{}, []string{arg})
+		s.Require().EqualError(err, "Unable to find team '"+arg+"'")
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Error while getting the team groups", func() {
+		printer.Clean()
+
+		arg := "teamId"
+		mockTeam := model.Team{Id: arg}
+		mockError := model.AppError{Message: "Mock error"}
+		groupOpts := model.GroupSearchOpts{
+			PageOpts: &model.PageOpts{
+				Page:    0,
+				PerPage: 10,
+			},
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam(arg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetGroupsByTeam(mockTeam.Id, groupOpts).
+			Return(nil, 0, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := teamGroupEnableCmdF(s.client, &cobra.Command{}, []string{arg})
+		s.Require().Equal(&mockError, err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("No groups on team", func() {
+		printer.Clean()
+
+		arg := "teamId"
+		mockTeam := model.Team{Id: arg}
+		groupOpts := model.GroupSearchOpts{
+			PageOpts: &model.PageOpts{
+				Page:    0,
+				PerPage: 10,
+			},
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam(arg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetGroupsByTeam(mockTeam.Id, groupOpts).
+			Return([]*model.Group{}, 0, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupEnableCmdF(s.client, &cobra.Command{}, []string{arg})
+		s.Require().EqualError(err, "Team '"+arg+"' has no groups associated. It cannot be group-constrained")
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Error patching the team", func() {
+		printer.Clean()
+
+		arg := "teamId"
+		mockTeam := model.Team{Id: arg}
+		mockError := model.AppError{Message: "Mock error"}
+		groupOpts := model.GroupSearchOpts{
+			PageOpts: &model.PageOpts{
+				Page:    0,
+				PerPage: 10,
+			},
+		}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(true)}
+
+		s.client.
+			EXPECT().
+			GetTeam(arg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetGroupsByTeam(mockTeam.Id, groupOpts).
+			Return([]*model.Group{&model.Group{}}, 1, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(mockTeam.Id, &teamPatch).
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := teamGroupEnableCmdF(s.client, &cobra.Command{}, []string{arg})
+		s.Require().Equal(&mockError, err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Successfully enable group", func() {
+		printer.Clean()
+
+		arg := "teamId"
+		mockTeam := model.Team{Id: arg}
+		groupOpts := model.GroupSearchOpts{
+			PageOpts: &model.PageOpts{
+				Page:    0,
+				PerPage: 10,
+			},
+		}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(true)}
+
+		s.client.
+			EXPECT().
+			GetTeam(arg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetGroupsByTeam(mockTeam.Id, groupOpts).
+			Return([]*model.Group{&model.Group{}}, 1, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(mockTeam.Id, &teamPatch).
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupEnableCmdF(s.client, &cobra.Command{}, []string{arg})
+		s.Require().NoError(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+}
