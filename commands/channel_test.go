cfcf8d40a82d640d1c17b31e3c0397bdb5aff982
phillipahereza <pahereza@gmail.com>
2019-12-30 15:54:12 +0300

    add channel creation tests

--- commands/channel_test.go
+++ commands/channel_test.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mmctl/printer"
-
 	"github.com/spf13/cobra"
 )
 
@@ -127,5 +126,152 @@ func (s *MmctlUnitTestSuite) TestSearchChannelCmdF() {
 		s.Len(printer.GetLines(), 0)
 		s.Len(printer.GetErrorLines(), 0)
 		s.EqualError(err, "Team "+teamArg+" was not found")
+	})
+}
+
+func (s *MmctlUnitTestSuite) TestCreateChannelCmdF(){
+	mockChannelName := "Mock Channel"
+	mockChannelDisplayName := "Mock Channel Display Name"
+	mockTeam := &model.Team{Name: "team", DisplayName: "team"}
+
+	s.Run("Create channel with no name returns an error", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		err := createChannelCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err.Error(), "Name is required")
+		s.Require().Len(printer.GetLines(), 0)
+
+	})
+
+	s.Run("Create channel with a name but no Display Name returns an error", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", mockChannelName, "")
+		err := createChannelCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err.Error(),"Display Name is required")
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Create channel with a Name but no Display Name returns an error", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("display_name", mockChannelDisplayName, "")
+		err := createChannelCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err.Error(),"Name is required")
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Create channel with a Name and Display Name but Team returns an error", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", mockChannelName, "")
+		cmd.Flags().String("display_name", mockChannelDisplayName, "")
+
+		err := createChannelCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err.Error(), "Team is required")
+		s.Require().Len(printer.GetLines(), 0)
+
+	})
+
+	s.Run("Test creating a channel with a nonexistent team returns an error", func() {
+		printer.Clean()
+
+		teamNameArg := "mockTeamName"
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", mockChannelName, "")
+		cmd.Flags().String("display_name", mockChannelDisplayName, "")
+		cmd.Flags().String("team", teamNameArg, "")
+
+		s.client.
+			EXPECT().
+			GetTeam(teamNameArg, "").
+			Return(nil, &model.Response{}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamNameArg, "").
+			Return(nil, &model.Response{}).
+			Times(1)
+
+		err := createChannelCmdF(s.client, cmd, []string{})
+
+		s.Require().Equal(err.Error(), "Unable to find team: "+ teamNameArg)
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Create open channel", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", mockChannelName, "")
+		cmd.Flags().String("display_name", mockChannelDisplayName, "")
+		cmd.Flags().String("team", mockTeam.Name, "")
+
+		mockChannel := &model.Channel{
+			Name:        mockChannelName,
+			DisplayName: mockChannelDisplayName,
+			TeamId: mockTeam.Id,
+			Type: model.CHANNEL_OPEN,
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam(mockTeam.Name, "").
+			Return(mockTeam, &model.Response{}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			CreateChannel(mockChannel).
+			Return(mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := createChannelCmdF(s.client, cmd, []string{})
+		s.Require().Nil(err)
+		s.Require().Equal(mockChannel, printer.GetLines()[0])
+		s.Require().Len(printer.GetLines(), 1)
+	})
+
+	s.Run("Create invite only channel", func() {
+		printer.Clean()
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", mockChannelName, "")
+		cmd.Flags().String("display_name", mockChannelDisplayName, "")
+		cmd.Flags().String("team", mockTeam.Name, "")
+		cmd.Flags().Bool("private", true, "")
+
+		mockChannel := &model.Channel{
+			Name:        mockChannelName,
+			DisplayName: mockChannelDisplayName,
+			TeamId: mockTeam.Id,
+			Type: model.CHANNEL_PRIVATE,
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam(mockTeam.Name, "").
+			Return(mockTeam, &model.Response{}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			CreateChannel(mockChannel).
+			Return(mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := createChannelCmdF(s.client, cmd, []string{})
+		s.Require().Nil(err)
+		s.Require().Equal(mockChannel, printer.GetLines()[0])
+		s.Require().Len(printer.GetLines(), 1)
 	})
 }
