1d22eebc45aee91565a1712a3c0b5a08339dca8a
Micah Thompson <mjthomp95@gmail.com>
2020-01-10 09:37:19 -0500

    [MM-20836] Add unit tests to the "TeamGroupDisableCmd" mmctl command (#76)
    * [MM-20836] Add unit tests to the "TeamGroupDisableCmd" mmctl command
    
    * Added mock error to check output error message
    
    Co-authored-by: Miguel de la Cruz <mgdelacroix@gmail.com>

--- commands/group_test.go
+++ commands/group_test.go
@@ -6,6 +6,7 @@ package commands
 import (
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mmctl/printer"
+
 	"github.com/spf13/cobra"
 )
 
@@ -170,8 +171,82 @@ func (s *MmctlUnitTestSuite) TestTeamGroupEnableCmd() {
 	})
 }
 
-func (s *MmctlUnitTestSuite) TestTeamGroupListCmd() {
+func (s *MmctlUnitTestSuite) TestTeamGroupDisableCmd() {
+	s.Run("Disable existing team", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(false)}
 
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(teamArg, &teamPatch).
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+	})
+
+	s.Run("Disable nonexisting team", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Unable to find team '"+teamArg+"'")
+	})
+
+	s.Run("Error response from PatchTeam", func() {
+		printer.Clean()
+		teamArg := "example-team-id"
+		mockTeam := model.Team{Id: teamArg}
+		teamPatch := model.TeamPatch{GroupConstrained: model.NewBool(false)}
+		errMessage := "PatchTeam Error"
+		mockError := &model.AppError{Message: errMessage}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchTeam(teamArg, &teamPatch).
+			Return(nil, &model.Response{Error: mockError}).
+			Times(1)
+
+		err := teamGroupDisableCmdF(s.client, &cobra.Command{}, []string{teamArg})
+		s.Require().NotNil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, mockError.Error())
+	})
+}
+
+func (s *MmctlUnitTestSuite) TestTeamGroupListCmd() {
 	s.Run("Team group list returns error when passing a nonexistent team", func() {
 		printer.Clean()
 
