df614a86fb30631e567456ee02c5c72984c96b14
Marc Argent <marc.argent@trustpower.com>
2020-01-13 22:12:54 +0000

    GH-13291 Add additional test

--- commands/group_test.go
+++ commands/group_test.go
@@ -465,4 +465,57 @@ func (s *MmctlUnitTestSuite) TestChannelGroupEnableCmdF() {
 		s.Require().Len(printer.GetErrorLines(), 0)
 		s.EqualError(err, "Unable to find channel '"+channelArg+"'")
 	})
+
+	s.Run("Enable group constraints with GetChannelByNameIncludeDeleted error", func() {
+		printer.Clean()
+
+		teamArg := "team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelPart := "channel-id"
+		mockChannel := model.Channel{Id: channelPart}
+		channelArg := teamArg + ":" + channelPart
+		group := &model.Group{Name: "group-name"}
+		mockGroups := []*model.Group{group}
+		groupOpts := &model.GroupSearchOpts{
+			PageOpts: &model.PageOpts{
+				Page:    0,
+				PerPage: 10,
+			},
+		}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByNameIncludeDeleted(channelPart, teamArg, "").
+			Return(nil, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannel(channelPart, "").
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetGroupsByChannel(channelPart, *groupOpts).
+			Return(mockGroups, 0, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchChannel(channelPart, &model.ChannelPatch{GroupConstrained: model.NewBool(true)}).
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := channelGroupEnableCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
 }
