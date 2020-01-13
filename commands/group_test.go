9f534c174ecb88bc6bca51961b1f29655803662f
larkox <larkox@gmail.com>
2020-01-13 04:21:36 -0500

    Add unit tests to the "ListLdapGroupsCmd" mmctl command (#84)

--- commands/group_test.go
+++ commands/group_test.go
@@ -10,6 +10,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func (s *MmctlUnitTestSuite) TestListLdapGroupsCmd() {
+	s.Run("Failure getting Ldap Groups", func() {
+		printer.Clean()
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			GetLdapGroups().
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := listLdapGroupsCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().Equal(&mockError, err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("List several groups", func() {
+		printer.Clean()
+		mockList := []*model.Group{
+			&model.Group{DisplayName: "Group1"},
+			&model.Group{DisplayName: "Group2"},
+			&model.Group{DisplayName: "Group3"},
+		}
+
+		s.client.
+			EXPECT().
+			GetLdapGroups().
+			Return(mockList, &model.Response{Error: nil}).
+			Times(1)
+
+		err := listLdapGroupsCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 3)
+		for i, v := range mockList {
+			s.Require().Equal(v, printer.GetLines()[i])
+		}
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+}
+
 func (s *MmctlUnitTestSuite) TestTeamGroupEnableCmd() {
 	s.Run("Enable unexisting team", func() {
 		printer.Clean()
