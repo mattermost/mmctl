2cc1417cbeff2afad79ae43bb5aabee6434557f9
Marc Argent <marc.argent@trustpower.com>
2020-01-14 21:28:58 +0000

    GH-13290 Add more test coverage

--- commands/group_test.go
+++ commands/group_test.go
@@ -1310,7 +1310,9 @@ func (s *MmctlUnitTestSuite) TestChannelGroupEnableCmdF() {
 }
 
 func (s *MmctlUnitTestSuite) TestChannelGroupDisableCmdF() {
-	s.Run("Disable group constrains with existing team and channel", func() {
+	s.Run("Disable group constraints with existing team and channel", func() {
+		printer.Clean()
+
 		teamArg := "team-id"
 		mockTeam := model.Team{Id: teamArg}
 		channelPart := "channel-id"
@@ -1341,8 +1343,9 @@ func (s *MmctlUnitTestSuite) TestChannelGroupDisableCmdF() {
 		s.Require().Len(printer.GetErrorLines(), 0)
 	})
 
-	s.Run("Disable group constrains with nonexistant team", func() {
+	s.Run("Disable group constraints with nonexistent team", func() {
 		printer.Clean()
+
 		teamArg := "team-id"
 		channelPart := "channel-id"
 		channelArg := strings.Join([]string{teamArg, channelPart}, ":")
@@ -1366,8 +1369,9 @@ func (s *MmctlUnitTestSuite) TestChannelGroupDisableCmdF() {
 		s.EqualError(err, "Unable to find channel '"+channelArg+"'")
 	})
 
-	s.Run("Disable group constrains with nonexistant channel", func() {
+	s.Run("Disable group constraints with nonexistent channel", func() {
 		printer.Clean()
+
 		teamArg := "team-id"
 		mockTeam := model.Team{Id: teamArg}
 		channelPart := "channel-id"
@@ -1396,5 +1400,141 @@ func (s *MmctlUnitTestSuite) TestChannelGroupDisableCmdF() {
 		s.Require().Len(printer.GetLines(), 0)
 		s.Require().Len(printer.GetErrorLines(), 0)
 		s.EqualError(err, "Unable to find channel '"+channelArg+"'")
+	})
+
+	s.Run("Disable group constraints with GetTeam error", func() {
+		printer.Clean()
+
+		teamArg := "team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelPart := "channel-id"
+		mockChannel := model.Channel{Id: channelPart}
+		channelArg := teamArg + ":" + channelPart
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByNameIncludeDeleted(channelPart, teamArg, "").
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchChannel(channelPart, &model.ChannelPatch{GroupConstrained: model.NewBool(false)}).
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		err := channelGroupDisableCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().Nil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Disable group constraints with GetTeamByName error", func() {
+		printer.Clean()
+
+		teamArg := "team-id"
+		channelPart := "channel-id"
+		channelArg := teamArg + ":" + channelPart
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetTeamByName(teamArg, "").
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := channelGroupDisableCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().NotNil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Unable to find channel '"+channelArg+"'")
+	})
+
+	s.Run("Disable group constraints with GetChannelByNameIncludeDeleted error", func() {
+		printer.Clean()
+
+		teamArg := "team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelPart := "channel-id"
+		channelArg := teamArg + ":" + channelPart
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByNameIncludeDeleted(channelPart, teamArg, "").
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannel(channelPart, "").
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := channelGroupDisableCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().NotNil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, "Unable to find channel '"+channelArg+"'")
+	})
+
+	s.Run("Disable group constraints with PatchChannel error", func() {
+		printer.Clean()
+
+		teamArg := "team-id"
+		mockTeam := model.Team{Id: teamArg}
+		channelPart := "channel-id"
+		mockChannel := model.Channel{Id: channelPart}
+		channelArg := teamArg + ":" + channelPart
+		mockError := model.AppError{Id: "Mock Error"}
+
+		s.client.
+			EXPECT().
+			GetTeam(teamArg, "").
+			Return(&mockTeam, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetChannelByNameIncludeDeleted(channelPart, teamArg, "").
+			Return(&mockChannel, &model.Response{Error: nil}).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			PatchChannel(channelPart, &model.ChannelPatch{GroupConstrained: model.NewBool(false)}).
+			Return(nil, &model.Response{Error: &mockError}).
+			Times(1)
+
+		err := channelGroupDisableCmdF(s.client, &cobra.Command{}, []string{channelArg})
+		s.Require().NotNil(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+		s.EqualError(err, mockError.Error())
 	})
 }
