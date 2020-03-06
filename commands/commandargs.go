db0183be6a86ae9936f87d3ab68a2c71f248440c
wiggin77 <wiggin77@warpmail.net>
2020-03-06 11:44:31 -0500

    MM-23031 support team:trigger syntax

--- commands/commandargs.go
+++ commands/commandargs.go
@@ -4,18 +4,57 @@
 package commands
 
 import (
+	"strings"
+
 	"github.com/mattermost/mmctl/client"
 
 	"github.com/mattermost/mattermost-server/v5/model"
 )
 
-// getCommandFromCommandArg retrieves a Command by command id. Future versions
-// may allow lookup by team:trigger
+// getCommandFromCommandArg retrieves a Command by command id or team:trigger.
 func getCommandFromCommandArg(c client.Client, commandArg string) *model.Command {
 	if checkSlash(commandArg) {
 		return nil
 	}
 
-	cmd, _ := c.GetCommandById(commandArg)
+	cmd := getCommandFromTeamTrigger(c, commandArg)
+	if cmd == nil {
+		cmd, _ = c.GetCommandById(commandArg)
+	}
+	return cmd
+}
+
+// getCommandFromTeamTrigger retrieves a Command via team:trigger syntax.
+func getCommandFromTeamTrigger(c client.Client, teamTrigger string) *model.Command {
+	if !strings.Contains(teamTrigger, ":") {
+		return nil
+	}
+
+	arr := strings.Split(teamTrigger, ":")
+	if len(arr) != 2 {
+		return nil
+	}
+
+	team, _ := c.GetTeamByName(arr[0], "")
+	if team == nil {
+		return nil
+	}
+
+	trigger := arr[1]
+	if len(trigger) == 0 {
+		return nil
+	}
+
+	var cmd *model.Command
+	list, _ := c.ListCommands(team.Id, false)
+	if list == nil {
+		return nil
+	}
+
+	for _, c := range list {
+		if c.Trigger == trigger {
+			cmd = c
+		}
+	}
 	return cmd
 }
