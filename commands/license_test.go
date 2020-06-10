cef5ae03df7b57f9349342b4dbf38425b8d3df77
Federico Martín Alconada Verzini <federico.alconada@icloud.com>
2020-06-10 05:22:46 -0300

    add unit tests to the UploadLicenseCmd mmctl command (#148)

--- commands/license_test.go
+++ commands/license_test.go
@@ -4,10 +4,18 @@
 package commands
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/spf13/cobra"
 
 	"github.com/mattermost/mmctl/printer"
+)
+
+const (
+	fakeLicensePayload = "This is the license."
 )
 
 func (s *MmctlUnitTestSuite) TestRemoveLicenseCmd() {
@@ -42,5 +50,51 @@ func (s *MmctlUnitTestSuite) TestRemoveLicenseCmd() {
 		s.Require().Len(printer.GetLines(), 0)
 		s.Require().Len(printer.GetErrorLines(), 0)
 		s.Require().Equal(err, mockErr)
+	})
+}
+
+func (s *MmctlUnitTestSuite) TestUploadLicenseCmdF() {
+	// create temporary file
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "testLicense-")
+	if err != nil {
+		panic(err)
+	}
+	text := []byte(fakeLicensePayload)
+	if _, err = tmpFile.Write(text); err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	mockLicenseFile := []byte(fakeLicensePayload)
+
+	s.Run("Upload license successfully", func() {
+		printer.Clean()
+		s.client.
+			EXPECT().
+			UploadLicenseFile(mockLicenseFile).
+			Return(true, &model.Response{Error: nil}).
+			Times(1)
+
+		err := uploadLicenseCmdF(s.client, &cobra.Command{}, []string{tmpFile.Name()})
+		s.Require().Nil(err)
+	})
+
+	s.Run("Fail to upload license if file not found", func() {
+		printer.Clean()
+		path := "/path/to/nonexistentfile"
+		errMsg := "open " + path + ": no such file or directory"
+		s.client.
+			EXPECT().
+			UploadLicenseFile(mockLicenseFile).
+			Times(0)
+
+		err := uploadLicenseCmdF(s.client, &cobra.Command{}, []string{path})
+		s.Require().EqualError(err, errMsg)
+	})
+
+	s.Run("Fail to upload license if no path is given", func() {
+		printer.Clean()
+		err := uploadLicenseCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().EqualError(err, fmt.Sprintf("enter one license file to upload"))
 	})
 }
