7b40a6654a17d4df9354eb0d932d7e951f4b0a4d
Ashish Bhate <bhate.ashish@gmail.com>
2020-07-31 14:33:53 +0000

    [MM-25647] add user delete command (#174)
    Summary
    - Add user delete command
     
    Ticket Link
    - https://mattermost.atlassian.net/browse/MM-25647

--- client/client.go
+++ client/client.go
@@ -59,6 +59,7 @@ type Client interface {
 	GetUser(userId, etag string) (*model.User, *model.Response)
 	GetUserByUsername(userName, etag string) (*model.User, *model.Response)
 	GetUserByEmail(email, etag string) (*model.User, *model.Response)
+	PermanentDeleteUser(userId string) (bool, *model.Response)
 	PermanentDeleteAllUsers() (bool, *model.Response)
 	CreateUser(user *model.User) (*model.User, *model.Response)
 	VerifyUserEmailWithoutToken(userId string) (*model.User, *model.Response)
