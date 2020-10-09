286d8e922c2ac41c41f3d34d5a996f9e8f9fb452
Shobhit Gupta <smartyshobhit@gmail.com>
2020-10-08 23:23:59 -0700

    Address comments

--- commands/team_e2e_test.go
+++ commands/team_e2e_test.go
@@ -6,6 +6,7 @@ package commands
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mmctl/client"
 	"github.com/mattermost/mmctl/printer"
 )
@@ -82,24 +83,10 @@ func (s *MmctlE2ETestSuite) TestDeleteTeamsCmdF() {
 		s.Require().Equal("Unable to delete team '"+s.th.BasicTeam.Name+"' error: : You do not have the appropriate permissions., ", printer.GetErrorLines()[0])
 	})
 
-	s.Run("Delete a valid team", func() {
+	s.RunForSystemAdminAndLocal("Delete a valid team", func(c client.Client) {
 		printer.Clean()
 
-		cmd := &cobra.Command{}
-		args := []string{s.th.BasicTeam.Name}
-		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
-		cmd.Flags().Bool("confirm", true, "")
-
-		err := deleteTeamsCmdF(s.th.LocalClient, cmd, args)
-		s.Require().Nil(err)
-		s.Len(printer.GetLines(), 1)
-		s.Equal(s.th.BasicTeam, printer.GetLines()[0])
-		s.Len(printer.GetErrorLines(), 0)
-	})
-
-	s.Run("Permission denied error for system admin when deleting a valid team", func() {
-		printer.Clean()
-		teamName := "teamname"
+		teamName := "teamname" + model.NewRandomString(10)
 		teamDisplayname := "Mock Display Name"
 		cmd := &cobra.Command{}
 		cmd.Flags().String("name", teamName, "")
@@ -107,16 +94,56 @@ func (s *MmctlE2ETestSuite) TestDeleteTeamsCmdF() {
 		err := createTeamCmdF(s.th.LocalClient, cmd, []string{})
 		s.Require().Nil(err)
 
-		printer.Clean()
-		args := []string{teamName}
 		cmd = &cobra.Command{}
+		args := []string{teamName}
 		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
 		cmd.Flags().Bool("confirm", true, "")
 
-		err = deleteTeamsCmdF(s.th.SystemAdminClient, cmd, args)
+		// Set EnableAPITeamDeletion
+		enableConfig := true
+		config, _ := c.GetConfig()
+		config.ServiceSettings.EnableAPITeamDeletion = &enableConfig
+		_, _ = c.UpdateConfig(config)
+
+		// Deletion should succeed for both local and SystemAdmin client now
+		err = deleteTeamsCmdF(c, cmd, args)
+		s.Require().Nil(err)
+		team := printer.GetLines()[0].(*model.Team)
+		s.Equal(teamName, team.Name)
+		s.Len(printer.GetErrorLines(), 0)
+
+		// Reset config
+		enableConfig = false
+		config, _ = c.GetConfig()
+		config.ServiceSettings.EnableAPITeamDeletion = &enableConfig
+		_, _ = c.UpdateConfig(config)
+	})
+
+	s.Run("Permission denied error for system admin when deleting a valid team", func() {
+		printer.Clean()
+
+		args := []string{s.th.BasicTeam.Name}
+		cmd := &cobra.Command{}
+		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
+		cmd.Flags().Bool("confirm", true, "")
+
+		// Delete should fail for SystemAdmin client
+		err := deleteTeamsCmdF(s.th.SystemAdminClient, cmd, args)
 		s.Require().Nil(err)
 		s.Len(printer.GetLines(), 0)
 		s.Len(printer.GetErrorLines(), 1)
-		s.Equal("Unable to delete team '"+teamName+"' error: : Permanent team deletion feature is not enabled. Please contact your System Administrator., ", printer.GetErrorLines()[0])
+		s.Equal("Unable to delete team '"+s.th.BasicTeam.Name+"' error: : Permanent team deletion feature is not enabled. Please contact your System Administrator., ", printer.GetErrorLines()[0])
+
+		// verify team still exists
+		team, _ := s.th.LocalClient.GetTeamByName(s.th.BasicTeam.Name, "")
+		s.Equal(team.Name, s.th.BasicTeam.Name)
+
+		// Delete should succeed for local client
+		printer.Clean()
+		err = deleteTeamsCmdF(s.th.LocalClient, cmd, args)
+		s.Require().Nil(err)
+		team = printer.GetLines()[0].(*model.Team)
+		s.Equal(team.Name, s.th.BasicTeam.Name)
+		s.Len(printer.GetErrorLines(), 0)
 	})
 }
