1135664843f1bbdf8ac03eb09beef664cc6a8347
jecepeda <jcepedavillamayor@gmail.com>
2020-10-11 01:10:53 +0200

    add tests

--- commands/team_e2e_test.go
+++ commands/team_e2e_test.go
@@ -6,6 +6,7 @@ package commands
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mmctl/client"
 	"github.com/mattermost/mmctl/printer"
 )
@@ -49,5 +50,50 @@ func (s *MmctlE2ETestSuite) TestRenameTeamCmdF() {
 		s.Require().Error(err)
 		s.Len(printer.GetLines(), 0)
 		s.Equal("Cannot rename team '"+s.th.BasicTeam.Name+"', error : : You do not have the appropriate permissions., ", err.Error())
+	})
+}
+
+func (s *MmctlE2ETestSuite) TestModifyTeamsCmdF() {
+	s.SetupTestHelper().InitBasic()
+	s.RunForSystemAdminAndLocal("system & local accounts can set a team to private", func(c client.Client) {
+		printer.Clean()
+		teamID := s.th.BasicTeam.Id
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("private", true, "")
+		err := modifyTeamsCmdF(c, cmd, []string{teamID})
+		s.Require().NoError(err)
+
+		t, appErr := s.th.App.GetTeam(teamID)
+		s.Require().Nil(appErr)
+		s.Require().Equal(model.TEAM_INVITE, t.Type)
+
+		// teardown
+		appErr = s.th.App.UpdateTeamPrivacy(teamID, model.TEAM_OPEN, true)
+		s.Require().Nil(appErr)
+	})
+	s.Run("user that creates the team can't set team's privacy due to permissions", func() {
+		printer.Clean()
+		teamID := s.th.BasicTeam.Id
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("private", true, "")
+		s.th.LoginBasic()
+		err := modifyTeamsCmdF(s.th.Client, cmd, []string{teamID})
+		s.Require().NoError(err)
+
+		t, appErr := s.th.App.GetTeam(teamID)
+		s.Require().Nil(appErr)
+		s.Require().Equal(model.TEAM_OPEN, t.Type)
+	})
+	s.Run("basic user with normal permissions that hasn't created the team can't set team's privacy", func() {
+		printer.Clean()
+		teamID := s.th.BasicTeam.Id
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("private", true, "")
+		s.th.LoginBasic2()
+		err := modifyTeamsCmdF(s.th.Client, cmd, []string{teamID})
+		s.Require().NoError(err)
+		t, appErr := s.th.App.GetTeam(teamID)
+		s.Require().Nil(appErr)
+		s.Require().Equal(model.TEAM_OPEN, t.Type)
 	})
 }
