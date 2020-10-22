d4b2a1eef097270b7a14429f4b97e48ebcf168b4
Shobhit Gupta <smartyshobhit@gmail.com>
2020-10-22 02:17:45 -0700

    Add end to end tests to the 'userConvertCmdF' mmctl command (#219)
    Co-authored-by: Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- commands/user_e2e_test.go
+++ commands/user_e2e_test.go
@@ -622,6 +622,100 @@ func (s *MmctlE2ETestSuite) TestDeleteUsersCmd() {
 	})
 }
 
+func (s *MmctlE2ETestSuite) TestUserConvertCmdF() {
+	s.SetupTestHelper().InitBasic()
+
+	s.RunForAllClients("Error when no flag provided", func(c client.Client) {
+		printer.Clean()
+
+		emailArg := "example@example.com"
+		cmd := &cobra.Command{}
+
+		err := userConvertCmdF(c, cmd, []string{emailArg})
+		s.Require().Error(err)
+		s.Require().Len(printer.GetLines(), 0)
+		s.Equal("either \"user\" flag or \"bot\" flag should be provided", err.Error())
+	})
+
+	s.RunForAllClients("Error for invalid user", func(c client.Client) {
+		printer.Clean()
+
+		emailArg := "something@something.com"
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("bot", true, "")
+
+		_ = userConvertCmdF(c, cmd, []string{emailArg})
+		s.Require().Len(printer.GetLines(), 0)
+	})
+
+	s.RunForSystemAdminAndLocal("Valid user to bot convert", func(c client.Client) {
+		printer.Clean()
+
+		user, _ := s.th.App.CreateUser(&model.User{Email: s.th.GenerateTestEmail(), Username: model.NewId(), Password: model.NewId()})
+
+		email := user.Email
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("bot", true, "")
+
+		err := userConvertCmdF(c, cmd, []string{email})
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 1)
+		bot := printer.GetLines()[0].(*model.Bot)
+		s.Equal(user.Username, bot.Username)
+		s.Equal(user.Id, bot.UserId)
+		s.Equal(user.Id, bot.OwnerId)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Permission error for valid user to bot convert", func() {
+		printer.Clean()
+
+		email := s.th.BasicUser2.Email
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("bot", true, "")
+
+		_ = userConvertCmdF(s.th.Client, cmd, []string{email})
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 1)
+		s.Equal(": You do not have the appropriate permissions., ", printer.GetErrorLines()[0])
+	})
+
+	s.RunForSystemAdminAndLocal("Valid bot to user convert", func(c client.Client) {
+		printer.Clean()
+
+		username := "fakeuser" + model.NewRandomString(10)
+		bot, _ := s.th.App.CreateBot(&model.Bot{Username: username, DisplayName: username, OwnerId: username})
+
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("user", true, "")
+		cmd.Flags().String("password", "password", "")
+
+		err := userConvertCmdF(c, cmd, []string{bot.Username})
+		s.Require().NoError(err)
+		s.Require().Len(printer.GetLines(), 1)
+		user := printer.GetLines()[0].(*model.User)
+		s.Equal(user.Username, bot.Username)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("Permission error for valid bot to user convert", func() {
+		printer.Clean()
+
+		username := "fakeuser" + model.NewRandomString(10)
+		bot, _ := s.th.App.CreateBot(&model.Bot{Username: username, DisplayName: username, OwnerId: username})
+
+		cmd := &cobra.Command{}
+		cmd.Flags().Bool("user", true, "")
+		cmd.Flags().String("password", "password", "")
+
+		err := userConvertCmdF(s.th.Client, cmd, []string{bot.Username})
+		s.Require().Error(err)
+		s.Equal(": You do not have the appropriate permissions., ", err.Error())
+		s.Require().Len(printer.GetLines(), 0)
+		s.Require().Len(printer.GetErrorLines(), 0)
+	})
+}
+
 func (s *MmctlE2ETestSuite) TestDeleteAllUserCmd() {
 	s.SetupTestHelper().InitBasic()
 
