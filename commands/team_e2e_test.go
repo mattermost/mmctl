87959d9ac43e7377b82d387c985e2d1e4109eb38
Shobhit Gupta <smartyshobhit@gmail.com>
2020-10-26 15:23:39 -0700

    Add end to end tests to the 'deleteTeamsCmdF' mmctl command (#218)
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- commands/team_e2e_test.go
+++ commands/team_e2e_test.go
@@ -56,6 +56,103 @@ func (s *MmctlE2ETestSuite) TestRenameTeamCmdF() {
 	})
 }
 
+func (s *MmctlE2ETestSuite) TestDeleteTeamsCmdF() {
+	s.SetupTestHelper().InitBasic()
+
+	s.RunForAllClients("Error deleting team which does not exist", func(c client.Client) {
+		printer.Clean()
+		nonExistentName := "existingName"
+		cmd := &cobra.Command{}
+		args := []string{nonExistentName}
+		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
+		cmd.Flags().Bool("confirm", true, "")
+
+		_ = deleteTeamsCmdF(c, cmd, args)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Require().Equal("Unable to find team '"+nonExistentName+"'", printer.GetErrorLines()[0])
+	})
+
+	s.Run("Permission error while deleting a valid team", func() {
+		printer.Clean()
+
+		cmd := &cobra.Command{}
+		args := []string{s.th.BasicTeam.Name}
+		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
+		cmd.Flags().Bool("confirm", true, "")
+
+		_ = deleteTeamsCmdF(s.th.Client, cmd, args)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Require().Equal("Unable to delete team '"+s.th.BasicTeam.Name+"' error: : You do not have the appropriate permissions., ", printer.GetErrorLines()[0])
+		team, _ := s.th.App.GetTeam(s.th.BasicTeam.Id)
+		s.Equal(team.Name, s.th.BasicTeam.Name)
+	})
+
+	s.RunForSystemAdminAndLocal("Delete a valid team", func(c client.Client) {
+		printer.Clean()
+
+		teamName := "teamname" + model.NewRandomString(10)
+		teamDisplayname := "Mock Display Name"
+		cmd := &cobra.Command{}
+		cmd.Flags().String("name", teamName, "")
+		cmd.Flags().String("display_name", teamDisplayname, "")
+		err := createTeamCmdF(s.th.LocalClient, cmd, []string{})
+		s.Require().Nil(err)
+
+		cmd = &cobra.Command{}
+		args := []string{teamName}
+		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
+		cmd.Flags().Bool("confirm", true, "")
+
+		// Set EnableAPITeamDeletion
+		enableConfig := true
+		config, _ := c.GetConfig()
+		config.ServiceSettings.EnableAPITeamDeletion = &enableConfig
+		_, _ = c.UpdateConfig(config)
+
+		// Deletion should succeed for both local and SystemAdmin client now
+		err = deleteTeamsCmdF(c, cmd, args)
+		s.Require().Nil(err)
+		team := printer.GetLines()[0].(*model.Team)
+		s.Equal(teamName, team.Name)
+		s.Len(printer.GetErrorLines(), 0)
+
+		// Reset config
+		enableConfig = false
+		config, _ = c.GetConfig()
+		config.ServiceSettings.EnableAPITeamDeletion = &enableConfig
+		_, _ = c.UpdateConfig(config)
+	})
+
+	s.Run("Permission denied error for system admin when deleting a valid team", func() {
+		printer.Clean()
+
+		args := []string{s.th.BasicTeam.Name}
+		cmd := &cobra.Command{}
+		cmd.Flags().String("display_name", "newDisplayName", "Team Display Name")
+		cmd.Flags().Bool("confirm", true, "")
+
+		// Delete should fail for SystemAdmin client
+		err := deleteTeamsCmdF(s.th.SystemAdminClient, cmd, args)
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Equal("Unable to delete team '"+s.th.BasicTeam.Name+"' error: : Permanent team deletion feature is not enabled. Please contact your System Administrator., ", printer.GetErrorLines()[0])
+
+		// verify team still exists
+		team, _ := s.th.App.GetTeam(s.th.BasicTeam.Id)
+		s.Equal(team.Name, s.th.BasicTeam.Name)
+
+		// Delete should succeed for local client
+		printer.Clean()
+		err = deleteTeamsCmdF(s.th.LocalClient, cmd, args)
+		s.Require().Nil(err)
+		team = printer.GetLines()[0].(*model.Team)
+		s.Equal(team.Name, s.th.BasicTeam.Name)
+		s.Len(printer.GetErrorLines(), 0)
+	})
+}
+
 func (s *MmctlE2ETestSuite) TestModifyTeamsCmdF() {
 	s.SetupTestHelper().InitBasic()
 
