d846056db8c5f6a103ac7b4fd6ec8833962bedb4
SezalAgrawal <sezalagrawal@gmail.com>
2020-10-27 14:29:05 +0530

    [MM-29561] Added e2e tests to the "listCommandCmdF (#252)
    * added e2e test cases for listCommandCmdF command
    
    * added more test cases
    
    * updated test cases
    
    * removed unnecessary file
    
    Co-authored-by: Sezal Agrawal <sezal@caringcompany.in>
    Co-authored-by: Miguel de la Cruz <miguel@mcrx.me>

--- commands/command_e2e_test.go
+++ commands/command_e2e_test.go
@@ -14,6 +14,153 @@ import (
 	"github.com/mattermost/mmctl/printer"
 )
 
+func (s *MmctlE2ETestSuite) TestListCommandCmd() {
+	s.SetupTestHelper().InitBasic()
+
+	s.RunForAllClients("List commands for a non existing team", func(c client.Client) {
+		printer.Clean()
+
+		nonexistentTeamID := "nonexistent-team-id"
+
+		err := listCommandCmdF(c, &cobra.Command{}, []string{nonexistentTeamID})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Equal("Unable to find team '"+nonexistentTeamID+"'", printer.GetErrorLines()[0])
+	})
+
+	s.RunForAllClients("List commands for a specific team", func(c client.Client) {
+		printer.Clean()
+
+		team, appErr := s.th.App.CreateTeam(&model.Team{
+			DisplayName: "dn_" + model.NewId(),
+			Name:        api4.GenerateTestTeamName(),
+			Email:       s.th.BasicUser.Email,
+			Type:        model.TEAM_OPEN,
+		})
+		s.Require().Nil(appErr)
+
+		_, appErr = s.th.App.AddUserToTeam(team.Id, s.th.BasicUser.Id, "")
+		s.Require().Nil(appErr)
+
+		command, appErr := s.th.App.CreateCommand(&model.Command{
+			DisplayName: "command",
+			CreatorId:   s.th.BasicUser.Id,
+			TeamId:      team.Id,
+			URL:         "http://localhost:8000/example",
+			Method:      model.COMMAND_METHOD_GET,
+			Trigger:     "trigger",
+		})
+		s.Require().Nil(appErr)
+		defer func() {
+			appErr = s.th.App.DeleteCommand(command.Id)
+			s.Require().Nil(appErr)
+		}()
+
+		err := listCommandCmdF(c, &cobra.Command{}, []string{team.Id})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 1)
+		s.Equal(command, printer.GetLines()[0])
+		s.Len(printer.GetErrorLines(), 0)
+	})
+
+	s.Run("List all commands from all teams", func() {
+		// add team1
+		team1, appErr := s.th.App.CreateTeam(&model.Team{
+			DisplayName: "dn_" + model.NewId(),
+			Name:        api4.GenerateTestTeamName(),
+			Email:       s.th.BasicUser.Email,
+			Type:        model.TEAM_OPEN,
+		})
+		s.Require().Nil(appErr)
+
+		_, appErr = s.th.App.AddUserToTeam(team1.Id, s.th.BasicUser.Id, "")
+		s.Require().Nil(appErr)
+
+		command1, appErr := s.th.App.CreateCommand(&model.Command{
+			DisplayName: "command1",
+			CreatorId:   s.th.BasicUser.Id,
+			TeamId:      team1.Id,
+			URL:         "http://localhost:8000/example",
+			Method:      model.COMMAND_METHOD_GET,
+			Trigger:     "trigger",
+		})
+		s.Require().Nil(appErr)
+		defer func() {
+			appErr = s.th.App.DeleteCommand(command1.Id)
+			s.Require().Nil(appErr)
+		}()
+
+		// add team 2
+		team2, appErr := s.th.App.CreateTeam(&model.Team{
+			DisplayName: "dn_" + model.NewId(),
+			Name:        api4.GenerateTestTeamName(),
+			Email:       s.th.BasicUser.Email,
+			Type:        model.TEAM_OPEN,
+		})
+		s.Require().Nil(appErr)
+
+		_, appErr = s.th.App.AddUserToTeam(team2.Id, s.th.BasicUser.Id, "")
+		s.Require().Nil(appErr)
+
+		command2, appErr := s.th.App.CreateCommand(&model.Command{
+			DisplayName: "command2",
+			CreatorId:   s.th.BasicUser.Id,
+			TeamId:      team2.Id,
+			URL:         "http://localhost:8000/example",
+			Method:      model.COMMAND_METHOD_GET,
+			Trigger:     "trigger",
+		})
+		s.Require().Nil(appErr)
+		defer func() {
+			appErr = s.th.App.DeleteCommand(command2.Id)
+			s.Require().Nil(appErr)
+		}()
+
+		s.RunForSystemAdminAndLocal("Run list command", func(c client.Client) {
+			printer.Clean()
+
+			err := listCommandCmdF(c, &cobra.Command{}, []string{})
+			s.Require().Nil(err)
+			s.Len(printer.GetLines(), 2)
+			s.ElementsMatch([]*model.Command{command1, command2}, printer.GetLines())
+			s.Len(printer.GetErrorLines(), 0)
+		})
+	})
+
+	s.Run("List commands for a specific team without permission", func() {
+		printer.Clean()
+
+		team, appErr := s.th.App.CreateTeam(&model.Team{
+			DisplayName: "dn_" + model.NewId(),
+			Name:        api4.GenerateTestTeamName(),
+			Email:       s.th.BasicUser.Email,
+			Type:        model.TEAM_OPEN,
+		})
+		s.Require().Nil(appErr)
+
+		command, appErr := s.th.App.CreateCommand(&model.Command{
+			DisplayName: "command",
+			CreatorId:   s.th.BasicUser.Id,
+			TeamId:      team.Id,
+			URL:         "http://localhost:8000/example",
+			Method:      model.COMMAND_METHOD_GET,
+			Trigger:     "trigger",
+		})
+		s.Require().Nil(appErr)
+		defer func() {
+			appErr = s.th.App.DeleteCommand(command.Id)
+			s.Require().Nil(appErr)
+		}()
+
+		err := listCommandCmdF(s.th.Client, &cobra.Command{}, []string{team.Id})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Equal("Unable to find team '"+team.Id+"'", printer.GetErrorLines()[0])
+	})
+}
+
 func (s *MmctlE2ETestSuite) TestArchiveCommandCmdF() {
 	s.SetupTestHelper().InitBasic()
 
