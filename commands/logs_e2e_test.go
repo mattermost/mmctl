2d8015024e6e2bea37deb5931dbc86058905a325
Jeremy Shih <jeremyshih0628@gmail.com>
2020-11-10 08:39:22 +0800

    Update commands/logs_e2e_test.go
    Co-authored-by: Miguel de la Cruz <mgdelacroix@gmail.com>

--- commands/logs_e2e_test.go
+++ commands/logs_e2e_test.go
@@ -29,6 +29,8 @@ func (s *MmctlE2ETestSuite) TestlogsCmdF() {
 		data, err := testLogsCmdF(s.th.SystemAdminClient, cmd, []string{})
 		s.Require().Nil(err)
 		s.Require().Len(data, 2)
-		s.Contains(data[1], "level=info msg=\"Syncing plugins from the file store\"")
+		s.Contains(data[1], "time=")
+		s.Contains(data[1], "level=")
+		s.Contains(data[1], "msg=")
 	})
 }
