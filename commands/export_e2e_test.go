a0b8307ea0033c6248871b1320e4704c7896a554
Martin Kraft <martin@upspin.org>
2021-05-04 15:06:07 -0400

    MM-32044: Fix shadow variable.

--- commands/export_e2e_test.go
+++ commands/export_e2e_test.go
@@ -326,12 +326,12 @@ func (s *MmctlE2ETestSuite) TestExportJobShowCmdF() {
 	s.Run("MM-T3885 - no permissions", func() {
 		printer.Clean()
 
-		job, appErr := s.th.App.CreateJob(&model.Job{
+		job2, appErr := s.th.App.CreateJob(&model.Job{
 			Type: model.JOB_TYPE_EXPORT_PROCESS,
 		})
 		s.Require().Nil(appErr)
 
-		err := exportJobShowCmdF(s.th.Client, &cobra.Command{}, []string{job.Id})
+		err := exportJobShowCmdF(s.th.Client, &cobra.Command{}, []string{job2.Id})
 		s.Require().EqualError(err, "failed to get export job: : You do not have the appropriate permissions., ")
 		s.Require().Empty(printer.GetLines())
 		s.Require().Empty(printer.GetErrorLines())
