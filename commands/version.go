1bb2fec4c15ec1e1ae67bf0d68787bfe8d023a94
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2021-12-21 16:30:52 +0100

    update mmctl to 6.4.0 which is the next release (#426)

--- commands/version.go
+++ commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	Version = "6.2.0"
+	Version = "6.4.0"
 	// SHA1 from git, output of $(git rev-parse HEAD)
 	gitCommit = "dev mode"
 	// State of git tree, either "clean" or "dirty"
