93752f29d878e5911c3f7273a59b60f361b3a526
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2022-03-23 18:39:45 +0300

    Makefile: get version from the git tags (#488)

--- commands/version.go
+++ commands/version.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	Version = "6.3.6"
+	Version = "unspecified"
 	// SHA1 from git, output of $(git rev-parse HEAD)
 	gitCommit = "dev mode"
 	// State of git tree, either "clean" or "dirty"
