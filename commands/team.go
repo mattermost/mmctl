24b2b01288c239959d4fc85617a9f2ee72288aa2
dsharma522 <50877070+dsharma522@users.noreply.github.com>
2022-10-25 01:32:11 +0530

    try go-lint fix

--- commands/team.go
+++ commands/team.go
@@ -358,12 +358,12 @@ func restoreTeamsCmdF(c client.Client, cmd *cobra.Command, args []string) error 
 	var result *multierror.Error
 	for i, team := range teams {
 		if team == nil {
-			result = multierror.Append(result, errors.New("Unable to find team '" + args[i] + "'"))
+			result = multierror.Append(result, errors.New("Unable to find team '"+args[i]+"'"))
 			printer.PrintError("Unable to find team '" + args[i] + "'")
 			continue
 		}
 		if rteam, _, err := c.RestoreTeam(team.Id); err != nil {
-			result = multierror.Append(result, errors.New("Unable to restore team '" + team.Name + "' error: " + err.Error()))
+			result = multierror.Append(result, errors.New("Unable to restore team '"+team.Name+"' error: "+err.Error()))
 			printer.PrintError("Unable to restore team '" + team.Name + "' error: " + err.Error())
 		} else {
 			printer.PrintT("Restored team '{{.Name}}'", rteam)
